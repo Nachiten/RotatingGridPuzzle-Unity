2bdfa4d8a62d9af9815623ce258651e3188f90b0
Ignacio Baptista [Desktop] <ibaptista@frba.utn.edu.ar>
2023-08-13 00:28:20 -0300

    Now Player can move continuously with a cooldown. And can switch directions pressing two keys

--- Assets/_Scripts/GridElement/Player.cs
+++ Assets/_Scripts/GridElement/Player.cs
@@ -2,32 +2,66 @@ using UnityEngine;
 
 public class Player : GridElement
 {
+    private float moveCooldownTimer;
+    private const float moveCooldownMax = 0.2f;
+    private bool isInCooldown;
+    private GridPosition moveDirection;
+    private Vector2 inputMoveDir;
+    
     protected override void Update()
     {
         base.Update();
         
         HandlePlayerInput();
+        HandlePlayerMovement();
     }
 
     private void HandlePlayerInput()
     {
         // Move according to InputManager GetPlayerMovementVector vector
-        Vector2 inputMoveDir = InputManager.Instance.GetPlayerMovementVector();
+        inputMoveDir = InputManager.Instance.GetPlayerMovementVector();
+    }
 
-        // There is no input
+    private void HandlePlayerMovement()
+    {
+        // If there is no input, reset cooldown
         if (inputMoveDir == Vector2.zero)
+        {
+            isInCooldown = false;
+            return;
+        }
+
+        if (!isInCooldown)
+        {
+            MovePlayer();
+            return;
+        }
+      
+        moveCooldownTimer -= Time.deltaTime;
+        if (moveCooldownTimer > 0f)
             return;
 
-        // If the player is moving, transport it to the previous target position
+        isInCooldown = false;
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        // If the player was already moving, teleport it to the previous target position
         if (isMoving)
             transform.position = targetPosition;
 
         // Calculate new position
         Vector3 moveDir = new(inputMoveDir.x * LevelGrid.Instance.GetCellSize(), 0f, inputMoveDir.y * LevelGrid.Instance.GetCellSize());
         Vector3 newPosition = transform.position + moveDir;
-        GridPosition newGridPosition = LevelGrid.Instance.GetGridPos(newPosition);
-        GridPosition direction = newGridPosition - centerGridPosition;
         
-        LevelGrid.Instance.TryMoveGridElements(GetGridPositionsForDirection(direction), direction);
+        // Calculate new grid position and direction
+        GridPosition newGridPosition = LevelGrid.Instance.GetGridPos(newPosition);
+        moveDirection = newGridPosition - centerGridPosition;
+        
+        LevelGrid.Instance.TryMoveGridElements(GetGridPositionsForDirection(moveDirection), moveDirection);
+        
+        isInCooldown = true;
+        moveCooldownTimer = moveCooldownMax;
     }
 }
