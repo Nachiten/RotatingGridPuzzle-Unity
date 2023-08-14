107c9ae3baf6d03e52196bd54acc315dcfaca9c0
Ignacio Baptista [Desktop] <ibaptista@frba.utn.edu.ar>
2023-08-14 12:18:21 -0300

    Removed unnecesary code

--- Assets/_Scripts/MouseWorld.cs
+++ Assets/_Scripts/MouseWorld.cs
@@ -2,22 +2,22 @@ using UnityEngine;
 
 public class MouseWorld : MonoBehaviour
 {
-    private static MouseWorld Instance;
-
-    [SerializeField] private LayerMask mousePlaneLayerMask;
-
-    private Camera mainCamera;
-
-    private void Awake()
-    {
-        Instance = this;
-        mainCamera = Camera.main;
-    }
-
-    private void Update()
-    {
-        transform.position = GetPositionOnlyHitVisible();
-    }
+    // private static MouseWorld Instance;
+    //
+    // [SerializeField] private LayerMask mousePlaneLayerMask;
+    //
+    // private Camera mainCamera;
+    //
+    // private void Awake()
+    // {
+    //     Instance = this;
+    //     mainCamera = Camera.main;
+    // }
+    //
+    // private void Update()
+    // {
+    //     transform.position = GetPositionOnlyHitVisible();
+    // }
 
     // private static Vector3 GetPosition()
     // {
@@ -27,22 +27,22 @@ public class MouseWorld : MonoBehaviour
     //     return raycastHit.point;
     // }
 
-    private static Vector3 GetPositionOnlyHitVisible()
-    {
-        Ray ray = Instance.mainCamera.ScreenPointToRay(InputManager.Instance.GetMouseScreenPosition());
-        
-        RaycastHit[] raycastHits = new RaycastHit[5];
-        var size = Physics.RaycastNonAlloc(ray, raycastHits, float.MaxValue, Instance.mousePlaneLayerMask);
-
-        System.Array.Sort(raycastHits, (x, y) => x.distance.CompareTo(y.distance));
-
-        foreach (RaycastHit raycastHit in raycastHits)
-        {
-            // If there is a renderer and it is enabled, return the point
-            if (raycastHit.transform.TryGetComponent(out Renderer renderer) && renderer.enabled)
-                return raycastHit.point;
-        }
-        
-        return Vector3.zero;
-    }
+    // private static Vector3 GetPositionOnlyHitVisible()
+    // {
+    //     Ray ray = Instance.mainCamera.ScreenPointToRay(InputManager.Instance.GetMouseScreenPosition());
+    //     
+    //     RaycastHit[] raycastHits = new RaycastHit[5];
+    //     var size = Physics.RaycastNonAlloc(ray, raycastHits, float.MaxValue, Instance.mousePlaneLayerMask);
+    //
+    //     System.Array.Sort(raycastHits, (x, y) => x.distance.CompareTo(y.distance));
+    //
+    //     foreach (RaycastHit raycastHit in raycastHits)
+    //     {
+    //         // If there is a renderer and it is enabled, return the point
+    //         if (raycastHit.transform.TryGetComponent(out Renderer renderer) && renderer.enabled)
+    //             return raycastHit.point;
+    //     }
+    //     
+    //     return Vector3.zero;
+    // }
 }
